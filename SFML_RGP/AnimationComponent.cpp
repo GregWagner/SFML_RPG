631623749d64971aff00ef97e8749d0b11b85764
GregWagner <theGreg101@gmail.com>
2019-05-02 10:35:38 -0500

    Continue with animation component

--- SFML_RGP/AnimationComponent.cpp
+++ SFML_RGP/AnimationComponent.cpp
@@ -1,6 +1,8 @@
 #include "AnimationComponent.h"
 
-AnimationComponent::AnimationComponent()
+AnimationComponent::AnimationComponent(sf::Sprite& sprite, sf::Texture& textureSheet)
+    : mSprite(sprite)
+    , mTextureSheet(textureSheet)
 {
 }
 
@@ -8,19 +10,55 @@ AnimationComponent::~AnimationComponent()
 {
 }
 
-AnimationComponent::Animation::Animation(sf::Texture& textureSheet, float speed, int startX, int startY, int endX, int endY,
+void AnimationComponent::addAnimation(std::string key)
+{
+}
+
+void AnimationComponent::startAnimation(std::string animation)
+{
+}
+
+void AnimationComponent::pauseAnimation(std::string animation)
+{
+}
+
+void AnimationComponent::resetAnimation(std::string animation)
+{
+}
+
+void AnimationComponent::update(const float& deltaTime)
+{
+}
+
+AnimationComponent::Animation::Animation(sf::Sprite& sprite, sf::Texture& textureSheet,
+    float animationTimer, int startX, int startY, int endX, int endY,
     int width, int height)
-    : mTextureSheet(textureSheet)
-    , mSpeed(speed)
+    : mSprite(sprite)
+    , mTextureSheet(textureSheet)
+    , mAnimationTimer(animationTimer)
     , mWidth(width)
     , mHeight(height)
 {
     mStartRect = sf::IntRect(startX, startY, width, height);
+    mCurrentRect = mStartRect;
     mEndRect = sf::IntRect(endX, endY, width, height);
+
+    mSprite.setTexture(textureSheet, true);
+    mSprite.setTextureRect(mStartRect);
 }
 
 void AnimationComponent::Animation::update(const float& deltatime)
 {
+    mTimer = 10.0f * deltatime;
+    if (mTimer >= mAnimationTimer) {
+        mTimer = 0.0f;
+
+        if (mCurrentRect != mEndRect) {
+            mCurrentRect.left += mWidth;
+        } else {
+            mCurrentRect.left = mStartRect.left;
+        }
+    }
 }
 
 void AnimationComponent::Animation::pause()
--- SFML_RGP/AnimationComponent.h
+++ SFML_RGP/AnimationComponent.h
@@ -6,25 +6,40 @@
 
 class AnimationComponent {
 public:
-    AnimationComponent();
+    AnimationComponent(sf::Sprite& sprite, sf::Texture& textureSheet);
     virtual ~AnimationComponent();
+
+    void addAnimation(std::string key);
+
+    void startAnimation(std::string animation);
+    void pauseAnimation(std::string animation);
+    void resetAnimation(std::string animation);
+
+    void update(const float& deltaTime);
 
 private:
     class Animation {
     public:
-        Animation(sf::Texture& textureSheet, float speed, int startX, int startY, int endX, int endY, int width, int height);
+        Animation(sf::Sprite& sprite, sf::Texture& textureSheet, float animationTimer, int startX, int startY, int endX, int endY, int width, int height);
 
         void update(const float& deltatime);
         void pause();
         void reset();
 
+        sf::Sprite& mSprite;
         sf::Texture& mTextureSheet;
-        float mSpeed {};
+        float mAnimationTimer {};
+        float mTimer {};
         int mWidth;
         int mHeight;
+
         sf::IntRect mStartRect {};
+        sf::IntRect mCurrentRect {};
         sf::IntRect mEndRect {};
     };
+
+    sf::Sprite& mSprite;
+    sf::Texture& mTextureSheet;
 
     std::map<std::string, Animation> animations;
 };
