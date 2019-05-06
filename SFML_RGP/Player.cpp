216e561e3a234d1d14227748ce921ebb355dea8e
GregWagner <theGreg101@gmail.com>
2019-05-06 08:27:38 -0500

    Animations for all directions

--- SFML_RGP/Player.cpp
+++ SFML_RGP/Player.cpp
@@ -8,8 +8,8 @@ Player::Player(float x, float y, sf::Texture& textureSheet)
     createAnimationComponent(textureSheet);
     createHitBoxComponent(mSprite, 86.0f, 70.0f, 86.0f, 111.0f);
 
-    mAnimationComponent->addAnimation("IDLE_LEFT", 10.0f, 0, 0, 13, 0, 192, 192);
-    mAnimationComponent->addAnimation("WALK_LEFT", 10.0f, 0, 1, 11, 1, 192, 192);
+    mAnimationComponent->addAnimation("IDLE", 10.0f, 0, 0, 13, 0, 192, 192);
+    mAnimationComponent->addAnimation("WALK", 10.0f, 0, 1, 11, 1, 192, 192);
 }
 
 Player::~Player()
@@ -20,9 +20,19 @@ void Player::update(const float& deltaTime)
 {
     mMovementComponet->update(deltaTime);
     if (mMovementComponet->getState(MovementStates::IDLE)) {
-        mAnimationComponent->play("IDLE_LEFT", deltaTime);
+        mAnimationComponent->play("IDLE", deltaTime);
     } else if (mMovementComponet->getState(MovementStates::MOVING_LEFT)) {
-        mAnimationComponent->play("WALK_LEFT", deltaTime);
+        mSprite.setOrigin(0.0f, 0.0f);
+        mSprite.setScale(1.0f, 1.0f);
+        mAnimationComponent->play("WALK", deltaTime);
+    } else if (mMovementComponet->getState(MovementStates::MOVING_RIGHT)) {
+        mSprite.setOrigin(258.0f, 0.0f);
+        mSprite.setScale(-1.0f, 1.0f);
+        mAnimationComponent->play("WALK", deltaTime);
+    } else if (mMovementComponet->getState(MovementStates::MOVING_UP)) {
+        mAnimationComponent->play("WALK", deltaTime);
+    } else if (mMovementComponet->getState(MovementStates::MOVING_DOWN)) {
+        mAnimationComponent->play("WALK", deltaTime);
     }
     mHixBoxComponent->update();
 }
